c5112bb09cc4e7f0273d787efa28c5495487c0a3
Md. Nurul Amin <mnurulamin111@gmail.com>
2026-09-06 14:30:16 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Smooth scrolling for navigation links
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function (e) {
+
+        const target = document.querySelector(
+            this.getAttribute("href")
+        );
+
+        if (target) {
+
+            e.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+
+        }
+
+    });
+
+});
